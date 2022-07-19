c6d87954066fa0a3d127574ba74d51af93e00969
borjagoc <borjagoc@gmail.com>
2022-07-19 16:59:54 +0100

    Goodbye route push in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
     def hello
         render html: "hello, world!"
     end
+    def goodbye
+        render html: "adios mundo cruel!"
+    end
 end
